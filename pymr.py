702c75a89c6adc8d50e3e72582e966c62487082e
kn45 <381404825@qq.com>
2016-07-28 19:47:11 +0800

    init ci

---
+++ pymr.py
@@ -0,0 +1,63 @@
+import sys
+from operator import itemgetter
+from itertools import imap, groupby
+from abc import ABCMeta
+
+
+class Mapper(object):
+    """override in subclass:
+    _setup
+    _map
+    _cleanup
+    """
+    def __init__(self):
+        return
+
+    def _setup(self):
+        return
+
+    def _map(self, inl):
+        print inl
+
+    def _cleanup(self):
+        return
+
+    def run(self):
+        self._setup()
+        for line in sys.stdin:
+            self._map(line.rstrip('\r\n'))
+        self._cleanup()
+
+
+class Reducer(object):
+    """override in subclass:
+    _setup
+    _map
+    _cleanup
+    """
+    def __init__(self):
+        return
+
+    def get_key(self, inl):
+        # extract the key from one data line
+        return inl.rstrip('\r\n').split('\t')[0]
+
+    def run(self):
+        self._setup()
+        stdin_strip = imap(lambda l: l.rstrip('\r\n'), sys.stdin)
+        for key, kvalues in groupby(stdin_strip, self.get_key):
+            self._reduce(key, kvalues)
+        self._cleanup()
+
+    def _setup(self):
+        return
+
+    def _reduce(self, key, kvalues):
+        for kv in kvalues:
+            print kv
+
+    def _cleanup(self):
+        return
+
+if __name__ == '__main__':
+    Reducer().run()
